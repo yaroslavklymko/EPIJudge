862b4451abe11758cb59fd7c103bad9fb3db9c35
Iaroslav Klymko <iaroslavk@starstechnologies.com>
2018-09-06 17:54:54 -0400

    [1.0.1. count_bits] Hamming weight solution

--- epi_judge_cpp/count_bits.cc
+++ epi_judge_cpp/count_bits.cc
@@ -2,7 +2,7 @@
 
 short CountBits(unsigned int x)
 {
-	// brute-force counting
+	/* naive solution
 	short result = 0;
 	while (x)
 	{
@@ -10,7 +10,13 @@ short CountBits(unsigned int x)
 		x >>= 1;
 	}
 
-	return result;
+	return result;*/
+
+	// Hamming weight solution
+	x -= (x >> 1) & 0x5555555555555555;             //put count of each 2 bits into those 2 bits
+	x = (x & 0x3333333333333333) + ((x >> 2) & 0x3333333333333333); //put count of each 4 bits into those 4 bits 
+	x = (x + (x >> 4)) & 0x0f0f0f0f0f0f0f0f;        //put count of each 8 bits into those 8 bits 
+	return (x * 0x0101010101010101) >> 56;  //returns left 8 bits of x + (x<<8) + (x<<16) + (x<<24
 }
 
 int main(int argc, char* argv[]) {
