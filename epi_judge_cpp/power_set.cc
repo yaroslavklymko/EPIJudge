cb4045924d383d5787a7ed77af745c4d781ff114
Iaroslav Klymko <iaroslavk@starstechnologies.com>
2018-09-09 18:15:12 -0400

    [5.5. power_set] "0..2^|S| - 1" solution, two cycles

--- epi_judge_cpp/power_set.cc
+++ epi_judge_cpp/power_set.cc
@@ -2,9 +2,20 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 
-vector<vector<int>> GeneratePowerSet(const vector<int>& input_set) {
-  // TODO - you fill in here.
-  return {};
+vector<vector<int>> GeneratePowerSet(const vector<int>& input_set)
+{
+	vector<vector<int>> result(1 << input_set.size());
+	for (int i = 0; i < (1 << input_set.size()); ++i)
+	{
+		for (int j = 0; j < input_set.size(); ++j)
+		{
+			if (i & (1 << j))
+			{
+				result[i].push_back(input_set[j]);
+			}
+		}
+	}
+	return std::move(result);
 }
 
 int main(int argc, char* argv[]) {
