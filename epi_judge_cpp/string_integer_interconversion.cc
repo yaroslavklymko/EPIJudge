896997eaca69b63b45d27d99020371b2755f72f0
Iaroslav Klymko <iaroslavk@starstechnologies.com>
2018-09-09 18:37:19 -0400

    [5.6. string_integer_interconversion] naive implementation - iterate over digits/characters

--- epi_judge_cpp/string_integer_interconversion.cc
+++ epi_judge_cpp/string_integer_interconversion.cc
@@ -3,14 +3,50 @@
 #include "test_framework/test_failure.h"
 using std::string;
 
-string IntToString(int x) {
-  // TODO - you fill in here.
-  return "";
+string IntToString(long long int x)
+{
+	string result;
+	const bool neg = (x < 0);
+	x = (x < 0) ? -x : x;
+
+	do 
+	{
+		result += '0' + (x % 10);
+		x /= 10;
+	}
+	while (x);
+
+	if (neg)
+	{
+		result += '-';
+	}
+
+	std::reverse(result.begin(), result.end());
+	return std::move(result);
 }
-int StringToInt(const string& s) {
-  // TODO - you fill in here.
-  return 0;
+
+long long int StringToInt(const string& s)
+{
+	long long int result = 0;
+	bool neg = false;
+
+	for (size_t i = 0; i < s.size(); ++i)
+	{
+		if (i == 0 && s[i] == '-')
+		{
+			neg = true;
+			continue;
+		}
+		if (s[i] < '0' || s[i] >'9')
+		{
+			throw std::exception(s.c_str());
+		}
+		result *= 10;
+		result += s[i] - '0';
+	}
+	return neg  ? -result : result;
 }
+
 void Wrapper(int x, const string& s) {
   if (IntToString(x) != s) {
     throw TestFailure("Int to string conversion failed");
